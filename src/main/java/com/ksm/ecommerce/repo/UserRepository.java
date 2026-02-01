a8a3e6dfd2953f82536b67b4643a3be58936114f
KSM <sunder007.ksm@gmail.com>
2026-02-01 13:29:10 +0530

    Added more methods to UserRepository.java

--- src/main/java/com/ksm/ecommerce/repo/UserRepository.java
+++ src/main/java/com/ksm/ecommerce/repo/UserRepository.java
@@ -4,8 +4,14 @@ import com.ksm.ecommerce.entity.User;
 import com.ksm.ecommerce.entity.embedded.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+    List<User> findByFirstNameIgnoreCase(String firstName);
+    List<User> findByLastNameIgnoreCase(String lastName);
+    List<User> findByEmailContainingIgnoreCase(String keyword);
+    Optional<User> findByPhone(String phone);
 }
